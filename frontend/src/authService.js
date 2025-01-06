567c2bb63c24dd1eed9678e8b76149fb68907819
prajwal-joshi45 <91948285+prajwal-joshi45@users.noreply.github.com>
2025-01-06 10:25:45 +0530

    Update authService.js

--- frontend/src/authService.js
+++ frontend/src/authService.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://final-val-backend.onrender.com/api';
 
 export const register = async (userData) => {
   const response = await fetch(`${API_URL}/auth/register`, {
